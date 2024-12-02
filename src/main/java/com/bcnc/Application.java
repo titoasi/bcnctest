a4c55865476d828d51e0fd2ed015b9bd7c130845
Asier Arostegui <asieraros91@hotmail.com>
2024-12-02 17:36:51 +0100

    Added main class

--- src/main/java/com/bcnc/Application.java
+++ src/main/java/com/bcnc/Application.java
@@ -1,4 +1,11 @@
 package com.bcnc;
 
-public class Application {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+    @SpringBootApplication
+    public class Application {
+        public static void main(String[] args) {
+            SpringApplication.run(Application.class, args);
+        }
 }
